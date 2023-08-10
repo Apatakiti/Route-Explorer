466153e06b4e2623531e5bbbb610b9198b94539b
ademola <ademolaowoeye07@gmail.com>
2023-08-10 11:11:49 +0100

    Terminating the loop from further visiting

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ function Delay() {
 const graph_adjMatrix = [];
 
 // Two dimensional graph (20 by 20)
-for (let index = 0; index < 6; index++) {
-  graph_adjMatrix[index] = new Array(6).fill(0);
+for (let index = 0; index < 11; index++) {
+  graph_adjMatrix[index] = new Array(11).fill(0);
 }
 
 CreateGrid = () => {
@@ -37,6 +37,7 @@ CreateGrid = () => {
 
 CreateGrid();
 
+// Not to go beyod the grid Range (- range)
 function validate_cell(
   nextVisitingRowNode,
   nextVisitingColNode,
@@ -51,24 +52,23 @@ function validate_cell(
   );
 }
 
-async function BFS_twoDimensionalGraph(graph) {
+async function BFS_2D_ShortestPath(graph, start_Coord_Node, target_Coord_Node) {
   const graph_Row = graph.length;
   const graph_Col = graph[0].length;
 
-  const visited = new Array(graph_Row)
-    .fill()
-    .map(() => new Array(graph_Col).fill(false));
+  const visited = new Array(graph_Row).fill().map(() => new Array(graph_Col).fill(false));
 
   // x and y direction coordinate (right, left, up, down)
   const directions = [
-    [1, 0],
-    [-1, 0],
-    [0, 1],
-    [0, -1],
+    [1, 0],  // down
+    [-1, 0], // up
+    [0, 1],  // right
+    [0, -1], // left
   ];
 
-  for (let startRow = 0; startRow < graph_Row; startRow++) {
-    for (let startCol = 0; startCol < graph_Col; startCol++) {
+  for (let startRow = start_Coord_Node[0]; startRow < graph_Row; startRow++) {
+    for (let startCol = start_Coord_Node[1]; startCol < graph_Col; startCol++) {
+
       if (!visited[startRow][startCol]) {
         visited[startRow][startCol] = true;
         const queue = [[startRow, startCol]];
@@ -78,13 +78,15 @@ async function BFS_twoDimensionalGraph(graph) {
           const [currentRow, currentCol] = queue.shift();
           visualizer([currentRow, currentCol]);
 
+          // Terminatine further node visiting on getting to the target node
+          if (currentRow === target_Coord_Node[0] && currentCol === target_Coord_Node[1]) {
+              return true
+          }
+
           for (const [directrow, directcol] of directions) {
             const nextRow = currentRow + directrow;
             const nextCol = currentCol + directcol;
-            if (
-              validate_cell(nextRow, nextCol, graph_Row, graph_Col) &&
-              !visited[nextRow][nextCol]
-            ) {
+            if (validate_cell(nextRow, nextCol, graph_Row, graph_Col) && !visited[nextRow][nextCol]) {
               visited[nextRow][nextCol] = true;
               queue.push([nextRow, nextCol]);
             }
@@ -95,4 +97,7 @@ async function BFS_twoDimensionalGraph(graph) {
   }
 }
 
-BFS_twoDimensionalGraph(graph_adjMatrix);
+let startN = [3, 3]
+let targetN = [5, 5]
+
+BFS_2D_ShortestPath(graph_adjMatrix, startN, targetN);
