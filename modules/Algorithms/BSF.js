87b35fae95fad67ac8bb4ec286c37cc3d9a8b926
ademola <ademolaowoeye07@gmail.com>
2023-08-18 20:53:45 +0100

    Traversing BSF file

---
+++ modules/Algorithms/BSF.js
@@ -0,0 +1,71 @@
+import { Grid_adjMatrix } from "../Grid-graph.js";
+import { Visualizer } from "../visualizer.js";
+
+export class BSF {
+  async TraverseBSF(startNode, targetNode) {
+    const grid_graph = new Grid_adjMatrix(11, 11);
+    const Visualize = new Visualizer();
+    grid_graph.obstacle(4, 5, 1);
+
+    const tableRow = grid_graph.matrix.length;
+    const tableCol = grid_graph.matrix[0].length;
+
+    const directions = [
+      [1, 0],
+      [-1, 0],
+      [0, 1],
+      [0, -1],
+    ];
+
+    const queue = [
+      {
+        currentRowNode: startNode[0],
+        currentColNode: startNode[1],
+        trackPath: [[startNode[0], startNode[1]]],
+      },
+    ];
+    const visited = new Set();
+
+    while (queue.length) {
+      const { currentRowNode, currentColNode, trackPath } = queue.shift();
+
+      visited.add(`${currentRowNode},${currentColNode}`);
+
+      // Delay to Visualize
+      await Visualize.delay();
+      Visualize.visiting(currentRowNode, currentColNode);
+
+      // Terminate/return shortest path on targetNode
+      if (
+        currentRowNode === targetNode[0] &&
+        currentColNode === targetNode[1]
+      ) {
+        for (const [currentRow, currentCol] of trackPath) {
+          await Visualize.delay()
+          Visualize.shortestPath(currentRow, currentCol)
+        }
+        return; // stop when path is found
+      }
+
+      for (const [directRow, directCol] of directions) {
+        let nextRow = currentRowNode + directRow;
+        let nextCol = currentColNode + directCol;
+        if (
+          nextRow >= 0 &&
+          nextCol >= 0 &&
+          tableRow > nextRow &&
+          tableCol > nextCol &&
+          grid_graph.matrix[nextRow][nextCol] === 0 &&
+          !visited.has(`${nextRow},${nextCol}`)
+        ) {
+          queue.push({
+            currentRowNode: nextRow,
+            currentColNode: nextCol,
+            trackPath: [...trackPath, [nextRow, nextCol]],
+          });
+        }
+      }
+    }
+    return; // No path found
+  }
+}
