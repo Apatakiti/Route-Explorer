2dd310c40ad1a4c5982adcbb09c2401c7b869ef6
ademola <ademolaowoeye07@gmail.com>
2023-08-25 04:11:24 +0100

    updated file and made some corrections

--- modules/Algorithms/DFS.js
+++ modules/Algorithms/DFS.js
@@ -3,7 +3,7 @@ import { Visualizer } from "../visualizer.js";
 const visualize = new Visualizer();
 
 export class DFS {
- async shortestPathDFS(adjMatrix, startNode, targetNode) {
+ async PathDFS(adjMatrix, startNode, targetNode) {
     const path = [];
     const visited = new Set();
 
@@ -15,32 +15,32 @@ export class DFS {
       }
   }
 
- async dfs(x, y, path, visited, adjMatrix, targetNode) {
+ async dfs(row, col, path, visited, adjMatrix, targetNode) {
     if (
-      x < 0                    ||
-      x >= adjMatrix.length    ||
-      y < 0                    ||
-      y >= adjMatrix[0].length ||
-      visited.has(`${x}-${y}`) ||
-      adjMatrix[x][y] !== 0
+      row < 0                      ||
+      row >= adjMatrix.length      ||
+      col < 0                      ||
+      col >= adjMatrix[0].length   ||
+      visited.has(`${row}-${col}`) ||
+      adjMatrix[row][col] !== 0
     ) {
       return false;
     }
 
-    path.push([x, y]);
-    visited.add(`${x}-${y}`);
+    path.push([row, col]);
+    visited.add(`${row}-${col}`);
 
-    if (x === targetNode[0] && y === targetNode[1]) {
+    if (row === targetNode[0] && col === targetNode[1]) {
       return true;
     }
 
     if (
-     await this.dfs(x + 1, y, path, visited, adjMatrix, targetNode) ||
-     await this.dfs(x - 1, y, path, visited, adjMatrix, targetNode) ||
-     await this.dfs(x, y + 1, path, visited, adjMatrix, targetNode) ||
-     await this.dfs(x, y - 1, path, visited, adjMatrix, targetNode)
+     await this.dfs(row + 1, col, path, visited, adjMatrix, targetNode) ||
+     await this.dfs(row - 1, col, path, visited, adjMatrix, targetNode) ||
+     await this.dfs(row, col + 1, path, visited, adjMatrix, targetNode) ||
+     await this.dfs(row, col - 1, path, visited, adjMatrix, targetNode)
     ) {
-      await visualize.delay()
+      // await visualize.delay()
       // This tracks path immediately and always not necessarily the shortest path
       // To visualize the recursive visitation
       // visualize.visiting(x, y)
