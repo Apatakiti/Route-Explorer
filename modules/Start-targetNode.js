3c1e6797ad83d5d6dc5befc35ef7840e94d88169
ademola <ademolaowoeye07@gmail.com>
2023-08-28 23:37:44 +0100

    control start and target globally from a button

--- modules/Start-targetNode.js
+++ modules/Start-targetNode.js
@@ -1,10 +1,9 @@
- const rows = 15
- const cols = 15
-   
-export const row_col = [rows, cols]
+const rows = 15;
+const cols = 30;
 
-const Random_Start_TargetNode = () => { 
+export const row_col = [rows, cols];
 
+export const Random_Start_TargetNode = () => {
   // Random StartNode falling on the left side of the grid
   const RandomStartCol = Math.floor(cols / 2 - (Math.random() * cols) / 2);
   const RandomStartRow = Math.floor(Math.random() * rows);
@@ -14,54 +13,56 @@ const Random_Start_TargetNode = () => {
   const RandomTargetRow = Math.floor(Math.random() * rows);
 
   return {
-      startNode: [RandomStartRow, RandomStartCol],
-      targetNode: [RandomTargetRow, RandomTargetCol]
-  }
+    startNode: [RandomStartRow, RandomStartCol],
+    targetNode: [RandomTargetRow, RandomTargetCol],
+  };
 };
 
-export const start_target = Random_Start_TargetNode()
+document.getElementById("ST").addEventListener("click", () => {
+  try {
+    new Promise(() => {
+      let startNode = document.querySelectorAll(".startNode");
+      let targetNode = document.querySelectorAll(".targetNode");
 
-// document.getElementById("start_targetNode").addEventListener("click", () => {
+      if (startNode && targetNode) {
+        setTimeout(() => {
+          startNode.forEach((ele) => {
+            ele.classList.remove("startNode");
+          });
 
-//   try {
-//     new Promise((resolve) => {
-//       let startNode = document.getElementById("startNode");
-//       let targetNode = document.getElementById("targetNode");
+          targetNode.forEach((ele) => {
+            ele.classList.remove("targetNode");
+          });
+        }, 50);
+      } else {
+        // throw new Error(
+        //   "Maybe Start Node OR Target Node Not Previously Assigned"
+        // );
+      }
+    });
+  } catch (error) {
+    console.error("Error", error.message);
+  }
 
-//       if (!startNode && !targetNode) {
-//         throw new Error(
-//           "Maybe Start Node OR Target Node Not Previously Assigned"
-//         );
-//       }
+  let start_target = Random_Start_TargetNode();
+  let startN = start_target.startNode;
+  let targetN = start_target.targetNode;
 
-//       setTimeout(() => {
-//         let removeStartN = startNode.removeAttribute("id");
-//         let removeTargetN = targetNode.removeAttribute("id");
-//         resolve(removeStartN, removeTargetN);
-//       }, 30);
-//     });
-//   } catch (error) {
-//     console.error("Error", error.message);
-//   }
+  new Promise(() => {
+    setTimeout(() => {
+      let getSN = document.getElementsByClassName(
+        `cell${startN[0]}${startN[1]}`
+      )[0];
+      let getTN = document.getElementsByClassName(
+        `cell${targetN[0]}${targetN[1]}`
+      )[0];
 
-//   let start_target = Random_Start_TargetNode();
-//   let startN = start_target.startNode;
-//   let targetN = start_target.targetNode;
-
-//   new Promise(() => {
-//     setTimeout(() => {
-//       let getSN = document.getElementById(`${"cell" + startN[0]}${startN[1]}`);
-//       let getTN = document.getElementById(
-//         `${"cell" + targetN[0]}${targetN[1]}`
-//       );
-
-//       getSN.setAttribute("id", "startNode");
-//       getTN.setAttribute("id", "targetNode");
-//     }, 60);
-//   })
-//     // .then()
-//     // .catch((error) => {
-//     //   console.error("Error:", error);
-//     // });
-
-
+      getSN.classList.add("startNode");
+      getTN.classList.add("targetNode");
+    }, 100);
+  });
+  // .then()
+  // .catch((error) => {
+  //   console.error("Error:", error);
+  // });
+});
