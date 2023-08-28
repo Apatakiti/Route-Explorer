970b1821f56ff97e63b8ac21c8d709acf1e2547d
ademola <ademolaowoeye07@gmail.com>
2023-08-28 23:37:25 +0100

    new graph file

---
+++ modules/graph.js
@@ -0,0 +1,23 @@
+  
+  let board = document.getElementById("board");
+  
+  // 2D graph for traversing visualization
+export const graph  = (rows, cols) => {
+    for (let x = 0; x < rows; x++) {
+      const tableRow = document.createElement("tr");
+      for (let y = 0; y < cols; y++) {
+        const tableCol = document.createElement("td");        
+
+        tableCol.setAttribute("id", `cell${x}${y}`);
+        tableCol.setAttribute("border", "2px");
+        tableCol.classList.add("cell", `cell${x}${y}`);
+        // let TextNode = document.createTextNode(`[${x}, ${y}]`);
+
+        // tableCol.appendChild(TextNode);
+        tableRow.appendChild(tableCol); 
+      }
+      tableRow.classList.add("cellRow")
+      board.appendChild(tableRow);
+    }    
+  }
+  
